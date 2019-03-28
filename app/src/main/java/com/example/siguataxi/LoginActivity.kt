0d5756a9ae6b8089fae93448b0be788e66de6a43
eduardocalix <eduardocalix11xtra@gmail.com>
2019-03-27 22:33:35 -0600

    creacion de cuenta en firebase

--- app/src/main/java/com/example/siguataxi/LoginActivity.kt
+++ app/src/main/java/com/example/siguataxi/LoginActivity.kt
@@ -1,10 +1,10 @@
 package com.example.siguataxi
 
-//import com.google.android.gms.auth.api.Auth
-//import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-//import com.google.android.gms.common.ConnectionResult
-//import com.google.android.gms.common.api.GoogleApiClient
-//import com.google.firebase.auth.FirebaseAuth
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.FirebaseAuth
 import android.support.v7.app.AppCompatActivity
 
 
@@ -12,11 +12,15 @@ import android.os.Bundle
 
 import android.content.Intent
 import android.support.v7.app.AlertDialog
+import android.text.TextUtils
+import android.view.View
 
 import android.widget.*
 
 import kotlinx.android.synthetic.main.activity_login.*
 import android.widget.Toast
+
+
 //import sun.security.jgss.GSSUtil.login
 
 
@@ -26,8 +30,14 @@ import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
 //"gladysberenicejimenez@gmail.com"
-    private val usuarioEmail:String = "eduardocalix11xtra@gmail.com"
-    private val contraseña:String="abc"
+    private lateinit var etUsuario:EditText
+    private lateinit var etContrasena:EditText
+    private lateinit var progressBar: ProgressBar
+    private lateinit var auth:FirebaseAuth
+
+
+//    private val usuarioEmail:String = "eduardocalix11xtra@gmail.com"
+//    private val contraseña:String="abc"
     //var mAuth: FirebaseAuth? = null
     //private val RC_SIGN_IN = 2
     //private var mGoogleApiClient: GoogleApiClient? = null
@@ -36,10 +46,10 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        var usuarioIngresado: EditText = findViewById(R.id.etUsuario)
-        usuarioIngresado.text.toString()
-        var contraseñauno: EditText = findViewById(R.id.etContraseña)
-        contraseñauno.text.toString()
+        etUsuario = findViewById(R.id.etUsuario)
+        etContrasena =  findViewById(R.id.etContrasena)
+        progressBar=findViewById(R.id.progressBar3)
+        auth=FirebaseAuth.getInstance()
         //val email = editTextEmail.getText().toString().trim()
         //val password = editTextPassword.getText().toString().trim()
 
@@ -57,13 +67,14 @@ class LoginActivity : AppCompatActivity() {
         }})*/
 
         btnIniciar.setOnClickListener {
-            if (usuarioIngresado.text.toString() == usuarioEmail && contraseñauno.text.toString() == contraseña) {
-                val intent = Intent(this, MenuActivity::class.java)
-                startActivity(intent)
-                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, R.string.contraseña_incorrecta, Toast.LENGTH_SHORT).show()
-            }
+//            if (usuarioIngresado.text.toString() == usuarioEmail && contraseñauno.text.toString() == contraseña) {
+//                val intent = Intent(this, MenuActivity::class.java)
+//                startActivity(intent)
+//                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(this, R.string.contraseña_incorrecta, Toast.LENGTH_SHORT).show()
+//            }
+            UsuarioInicio()
         }
 
         tvRegistro1.setOnClickListener {
@@ -72,6 +83,38 @@ class LoginActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    fun olvideContraseña(view: View){
+
+    }
+    fun login(view: View){
+
+    }
+    private fun UsuarioInicio(){
+        val usuario:String=etUsuario.text.toString()
+        val contrasena:String=etContrasena.text.toString()
+
+        if (!TextUtils.isEmpty(usuario)&& !TextUtils.isEmpty(contrasena)){
+            progressBar.visibility=View.VISIBLE
+
+            auth.signInWithEmailAndPassword(usuario,contrasena)
+                .addOnCompleteListener(this){
+                    task ->
+                    if (task.isSuccessful){
+                    action()
+                    }else{
+                        Toast.makeText(this, "Error en la autenticacion", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+        }
+    }
+
+    private fun action(){
+        val intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
+        Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
     }
     override fun onBackPressed() {
 
--- app/src/main/java/com/example/siguataxi/RegistroActivity.kt
+++ app/src/main/java/com/example/siguataxi/RegistroActivity.kt
@@ -1,52 +1,64 @@
 package com.example.siguataxi
 
-import android.content.DialogInterface
 import android.content.Intent
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-import android.support.v7.app.AlertDialog
+import android.text.TextUtils
+import android.view.View
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 import kotlinx.android.synthetic.main.activity_registro.*
 
 
-
-
-
-
 class RegistroActivity : AppCompatActivity() {
+
+    private lateinit var etNombre:EditText
+    private lateinit var etApellidos:EditText
+    private lateinit var etCorreo:EditText
+    private lateinit var etContrasena:EditText
+    private lateinit var etTelefono:EditText
+    private lateinit var dbReference: DatabaseReference
+    private lateinit var database: FirebaseDatabase
+    private lateinit var progressBar: ProgressBar
+    private lateinit var auth:FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
 
+        etNombre = findViewById(R.id.etNombre)
+        etApellidos = findViewById(R.id.etApellidos)
+        etTelefono = findViewById(R.id.etTelefono)
+        etCorreo= findViewById(R.id.etCorreo)
+        etContrasena = findViewById(R.id.etContrasena)
+        progressBar = findViewById(R.id.progressBar2)
+        database = FirebaseDatabase.getInstance()
+        auth= FirebaseAuth.getInstance()
+        dbReference = database.reference.child("User")
+
         //Evalua que radio boton esta seleccionado para la cracion
         // de los usuarios ya sea taxista o usuario normal
 
-        var nombre: EditText = findViewById(R.id.etNombre)
-        nombre.text.toString()
-        var usuarioIngresado: EditText = findViewById(R.id.etCorreo)
-        usuarioIngresado.text.toString()
-        var contraseñauno: EditText = findViewById(R.id.etContraseña)
-        contraseñauno.text.toString()
-        var contraseñados: EditText = findViewById(R.id.etConfirmar)
-        contraseñados.text.toString()
-        var telefono: EditText = findViewById(R.id.etTelefono)
-        telefono.text.toString()
+
+
         rbTaxi.setOnClickListener{
         if(rbTaxi.isChecked ){
             rbUsuario.isChecked=false
             btncuenta.setText (R.string.siguiente)
             btncuenta.setOnClickListener {
-                if(nombre.text.toString() != "" && usuarioIngresado.text.toString()!="" && contraseñauno.text.toString()!="" && contraseñados.text.toString()!="" && telefono.text.toString()!=""){
-
                     val intent = Intent(this, TaxiActivity::class.java)
-                startActivity(intent)}else{
-                    Toast.makeText(this, "Faltan campos por llenar", Toast.LENGTH_SHORT).show()
-                }
+                    startActivity(intent)
+                   // Toast.makeText(this, "Faltan campos por llenar", Toast.LENGTH_SHORT).show()
+
 
             }
 
@@ -60,13 +72,10 @@ class RegistroActivity : AppCompatActivity() {
             if(rbUsuario.isChecked){
                 rbTaxi.isChecked=false
                 btncuenta.setText(R.string.crear_cuenta)
-                btncuenta.setOnClickListener {
-                    if(nombre.text.toString() != "" && usuarioIngresado.text.toString()!="" && contraseñauno.text.toString()!="" && contraseñados.text.toString()!="" && telefono.text.toString()!=""){
 
-                        Toast.makeText(this, R.string.usuarioCreado, Toast.LENGTH_SHORT).show()
-                }else{
-                    Toast.makeText(this, "Faltan campos por llenar", Toast.LENGTH_SHORT).show()
-                }}
+        btncuenta.setOnClickListener {
+                crearNuevaCuenta()
+          }
             }
         }
 
@@ -75,9 +84,55 @@ class RegistroActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
     }
+   fun registro(view: View){
+       crearNuevaCuenta()
+    }
+    private  fun crearNuevaCuenta(){
+        val nombre:String=etNombre.text.toString()
+        val apellido:String= etApellidos.text.toString()
+        val correo:String= etCorreo.text.toString()
+        val contrasena : String=etContrasena.text.toString()
+        val confirmar : String=etConfirmar.text.toString()
+        val telefono:String=etTelefono.text.toString()
 
+        if(!TextUtils.isEmpty(nombre)&&!TextUtils.isEmpty(apellido)&&!TextUtils.isEmpty(contrasena)&&!TextUtils.isEmpty(correo)&&!TextUtils.isEmpty(telefono)&&contrasena==confirmar) {
+            progressBar.visibility= View.VISIBLE
 
+            auth.createUserWithEmailAndPassword(correo,contrasena)
+                .addOnCompleteListener (this){
+                    task ->
 
+                    if (task.isComplete){
+                        val user: FirebaseUser?=auth.currentUser
+                        verifyEmail(user)
+                        Toast.makeText(this, "esperando...", Toast.LENGTH_LONG).show()
 
+                        val userBD=dbReference.child(user?.uid.toString())
+                        userBD.child("nombres").setValue(nombre).toString()
+                        userBD.child("apellidos").setValue(apellido).toString()
+                        userBD.child("telefono").setValue(telefono).toString()
+
+                        action()
+                    }
+                }
+
+        }
+    }
+    private fun action(){
+        startActivity(Intent(this,LoginActivity::class.java))
+    }
+    private fun verifyEmail(user:FirebaseUser?) {
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener(this) { task ->
+
+                if (task.isComplete) {
+                    Toast.makeText(this, "Email enviado", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Error al enviar Email", Toast.LENGTH_LONG).show()
+
+                }
+            }
+    }
 }
